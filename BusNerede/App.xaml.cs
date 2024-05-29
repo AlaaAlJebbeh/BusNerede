fadb1e557b4ead30e59c986d56ba4efc741b1f6a
AlaaAlJebbeh <alaa.jeebeh@gmail.com>
2024-05-29 13:32:11 +0300

    Add project files.

---
+++ BusNerede/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BusNerede
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BusNerede/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BusNerede
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
